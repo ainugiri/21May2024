496cb6f7b286e8911c1a8a448cefc5a642ecf756
Giri Prasad P <giriprasa@gmail.com>
2024-05-22 12:03:54 +0530

    cr215

--- index.cpp
+++ index.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 int main() {
+    // Print "Hello, World!" to the console
+    // std::cout is the standard output stream
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
